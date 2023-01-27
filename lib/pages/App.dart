36362b413d6d5afb1bc214fd35241e7ea1b78fd9
Ale Espinosa <espinosacordovaaleff@gmail.com>
2023-01-27 10:49:10 -0600

    register and login

---
+++ lib/pages/App.dart
@@ -0,0 +1,93 @@
+import 'package:a_app/pages/Register.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => AppState();
+}
+
+class AppState extends State<App> {
+  late String _user;
+  late String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 90.0),
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 100.0,
+                width: 100.0,
+                child: Image.asset('assets/images/login.jpg'),
+              ),
+              const Text(
+                'Login',
+                style: TextStyle(fontSize: 40.0),
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'USERNAME',
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (valor) {
+                  _user = valor;
+                  print('Usuario: $_user');
+                },
+              ),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                obscureText: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'PASSWORD',
+                    labelText: 'password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (valor) {
+                  _password = valor;
+                  print('Password: $_password');
+                },
+              ),
+              const Divider(
+                height: 40.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Presiono el boton');
+                },
+                child: const Text(
+                  'Sing in',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Registrarse'),
+                onPressed: () {
+                  MaterialPageRoute route =
+                      MaterialPageRoute(builder: (context) => const Register());
+                  Navigator.push(context, route);
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/Register.dart
@@ -0,0 +1,166 @@
+import 'package:a_app/pages/App.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register({super.key});
+
+  get gender => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 90.0),
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 100.0,
+                width: 100.0,
+                child: Image.asset('assets/images/login.jpg'),
+              ),
+              const Text(
+                'Register',
+                style: TextStyle(fontSize: 40.0),
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'USERNAME',
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (value) {},
+              ),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'LAST NAME',
+                    labelText: 'last name',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (value) {},
+              ),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'FIRST NAME',
+                    labelText: 'first name',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (value) {},
+              ),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'PHONE NUMBER',
+                    labelText: 'Number',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (value) {},
+              ),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                  enableInteractiveSelection: false,
+                  obscureText: true,
+                  textCapitalization: TextCapitalization.characters,
+                  decoration: InputDecoration(
+                      hintText: 'PASSWORD',
+                      labelText: 'password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0))),
+                  onSubmitted: (value) {}),
+              const Divider(
+                height: 20.0,
+              ),
+              TextField(
+                enableInteractiveSelection: false,
+                autofocus: true,
+                textCapitalization: TextCapitalization.characters,
+                decoration: InputDecoration(
+                    hintText: 'CONFIRM PASSWORD',
+                    labelText: 'confirm password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                onSubmitted: (value) {},
+              ),
+              const Divider(
+                height: 50.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: DropdownButtonFormField(
+                        validator: (value) =>
+                            value == "Sexo" ? "campo requerido" : null,
+                        value: gender,
+                        items: ["Femenino", "Masculino", "Otro"]
+                            .map((e) => DropdownMenuItem(
+                                  value: e,
+                                  child: Text(e),
+                                ))
+                            .toList(),
+                        onChanged: (Object? value) {},
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 150,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Nacimiento',
+                        labelText: 'Nacimiento',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Registrarse',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Iniciar sesion'),
+                onPressed: () {
+                  MaterialPageRoute route =
+                      MaterialPageRoute(builder: (context) => const App());
+                  Navigator.push(context, route);
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
